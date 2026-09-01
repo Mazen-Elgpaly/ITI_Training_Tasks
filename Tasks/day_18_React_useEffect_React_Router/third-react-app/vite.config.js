3bb6ed79d503107fa9feb1b5ad5640e6fa2ae69f
Mazen ElGebaly <mazenelgpaly180@gmail.com>
2026-09-01 10:50:22 -0700

    commit

--- Tasks/day_18_React_useEffect_React_Router/third-react-app/vite.config.js
+++ Tasks/day_18_React_useEffect_React_Router/third-react-app/vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Tasks/day_18_React_useEffect_&_React_Router/third-react-app/",
+  base: "/Tasks/day_18_React_useEffect_React_Router/third-react-app/",
 });
